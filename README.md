26d865d819c9cf8636ae493877b71d419d4819d5
Houda <n_medelfef@ensta.edu.dz>
2025-12-01 11:07:36 +0100

    Preprocessing et préparation régression linéaire

---
+++ README.md
@@ -0,0 +1,9 @@
+# USA House Price Prediction - Decision Tree Model
+
+Projet complet de Machine Learning :
+- Prétraitement des données
+- Entraînement 
+- Évaluation du modèle
+- Optimisation
+- Prédiction sur nouvelles données
+- Structure professionnelle
--- src/preprocess.py
+++ src/preprocess.py
@@ -1,4 +1,16 @@
 
 import pandas as pd
-df = pd.read_csv("../data/raw/USA Housing Dataset.csv")
+df = pd.read_csv("./data/raw/USA Housing Dataset.csv")
 print(df.head())
+print(df.shape)
+print(df.info()) 
+print(df.columns)
+
+
+df["date"] = pd.to_datetime(df["date"])
+
+df["year_sold"] = df["date"].dt.year
+df["month_sold"] = df["date"].dt.month
+df["day_sold"] = df["date"].dt.day
+
+#df.drop(columns=["date"], inplace=True)
